5ea551c616b62e2063dc01bda1acedde7e82be65
Beto <rofilho@jc.com.br>
2018-11-13 13:50:02 -0200

    comite babel

--- resources/assets/js/layout/topo.vue
+++ resources/assets/js/layout/topo.vue
@@ -7,7 +7,18 @@
 <script>
     export default {
         mounted() {
-
+            axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            .then(function (response) {
+              // handle success
+              console.log(response);
+            })
+            .catch(function (error) {
+              // handle error
+              console.log(error);
+            })
+            .then(function () {
+              // always executed
+            });
         }
     }
 </script>
