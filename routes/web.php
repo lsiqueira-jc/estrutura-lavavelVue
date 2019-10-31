01ecb186adab79e1410295e6b504367653b82812
Lucas Menezes <49875735+lvmenezes@users.noreply.github.com>
2019-10-31 17:03:53 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Aqui é onde você pode registrar rotas da web para seu aplicativo. Estes
+| rotas são carregadas pelo RouteServiceProvider dentro de um grupo que
+| contém o grupo de middleware "web". Agora crie algo ótimo!
 |
 */
 
